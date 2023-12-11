14e5376cde8f188c0afd1a67ea392349ce76f5c4
Jugurtha AIT OUFELLA <jugurtha.aitoufella@ext.leroymerlin.fr>
2023-12-11 13:55:39 +0100

    chore: init front end

---
+++ front/layouts/headers.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="container">
+    <header>
+      <h1>Header</h1>
+    </header>
+
+    <main>
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
