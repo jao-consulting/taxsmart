fa5146020d808863954b3a07508bfa166e6fdf86
Jugurtha AIT OUFELLA <jugurtha.aitoufella@ext.leroymerlin.fr>
2023-12-22 17:08:32 +0100

    chore: init backend module

---
+++ back/taxsmart--bff/src/main/java/com/jao/taxsmart/bff/BackendForFrontRestApplication.java
@@ -0,0 +1,11 @@
+package com.jao.taxsmart.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendForFrontRestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BackendForFrontRestApplication.class, args);
+	}
+}
