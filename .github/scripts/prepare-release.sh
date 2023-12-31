39595e1b860a1ec61d1be6b3899fe7e9b13fcfe8
Jugurtha AIT OUFELLA <jugurtha.aitoufella@ext.leroymerlin.fr>
2023-12-31 15:52:32 +0100

    chore: updating semantic release process updating current version in pom and package

--- .github/scripts/prepare-release.sh
+++ .github/scripts/prepare-release.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo "version = $1"
+
+# Get version number from version tag
+VERSION=$1
+
+cd back
+mvn versions:set-property -Dproperty=revision -DnewVersion="$VERSION"
+cd ../
+
+cd front
+npm version --allow-same-version --no-git-tag-version "$VERSION"
+cd ../
