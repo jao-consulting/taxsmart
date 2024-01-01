51c34cb1b39cb8900ecb1f9525debc14c2dbb35b
Jugurtha AIT OUFELLA <jugurtha.aitoufella@ext.leroymerlin.fr>
2024-01-01 10:41:02 +0100

    docs: documenting dependency management and delivery components

--- front/front-entrypoint.sh
+++ front/front-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+# This script is used to start the front-end server of the application.
+
+# The NITRO_PORT environment variable is set to "8080". This is the port that the server will listen on.
 export NITRO_PORT="8080"
 
+# The node command is used to start the server. The server code is located in the .output/server/index.mjs file.
 node .output/server/index.mjs
