14373c65aa41f4ec11e59b2b332818c069548090
Jugurtha AIT OUFELLA <jugurtha.aitoufella@ext.leroymerlin.fr>
2024-01-01 10:41:02 +0100

    docs: documenting dependency management and delivery components

--- back/back-entrypoint.sh
+++ back/back-entrypoint.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
+# This script is used to start the back-end server of the application.
+
+# The java command is used to start the server. The server code is packaged in the app.jar file.
 java -jar app.jar
